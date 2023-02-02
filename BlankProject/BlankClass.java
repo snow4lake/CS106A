590f8002a7f3c25aba7745d9f414e2a224a52b5d
snow4lake <geta.copil@gmail.com>
2023-02-02 11:05:36 +0200

    initial commit

---
+++ BlankProject/BlankClass.java
@@ -0,0 +1,17 @@
+/*
+ * File: BlankClass.java
+ * ---------------------
+ * This class is a blank one that you can change at will. Remember, if you change
+ * the class name, you'll need to change the filename so that it matches.
+ * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
+ */
+
+import acm.program.*;
+
+@SuppressWarnings("serial")
+public class BlankClass extends ConsoleProgram {
+	public void run() {
+		/* You fill this in */
+	}
+}
+
